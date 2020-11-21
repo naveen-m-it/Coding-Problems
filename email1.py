d69bf975bf6a94aa55160e50edfbea62348d60d2
Naveen M <naveenmnka15@gmail.com>
2020-11-21 13:34:50 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -5,12 +5,13 @@ import smtplib
 
 #this is a sample method for working sample
 #use your email and default password to send mail
-sender = ("example@gmail.com")
+sender = ("naveen.sample.work@gmail.com")
 recv = input("Enter email to send:")
 message=input("Enter message:")
 pas = input("enter password:")
 
 #there is i use gmail smtp you also using other company smtps for other email 
+#if this port not work use 25 or 465
 
 s = smtplib.SMTP('smtp.gmail.com',587)
 
@@ -27,3 +28,5 @@ try:
 except:
     print("mail not send")
 s.quit()#close the program
+#is Closed error accured change port number
+# to get authendication failure change your sender email's security->low security app- on this option to login
